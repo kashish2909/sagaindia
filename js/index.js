4c0f78dee22364b89747931a3de8ed90820b97b6
Maitree Leekha <maitreeleekha@yahoo.in>
2018-01-15 11:19:02 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+var images=['img/saga8.jpeg','img/saga6.jpeg','img/saga7.jpeg'];
+var i=0;
+$('#intro').backstretch([
+    "img/saga1.jpeg"
+  , "img/saga6.jpeg"
+  , "img/saga8.jpeg"
+ ], {duration: 3000, fade: 750});
+ 
+
+window.onload=init;
+function init(){
+
+}
