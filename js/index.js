496e05b27262be3d7cd357f45bb54d80cf7cb9fc
kashish <bansal.kashish6@gmail.com>
2018-03-28 13:54:02 +0530

    updated

--- js/index.js
+++ js/index.js
@@ -1,13 +1,23 @@
-var images=['img/saga8.jpeg','img/saga6.jpeg','img/saga7.jpeg'];
+//var images=['img/factory/1.jpg','img/factory/2.jpg','img/factory/3.jpg'];
 var i=0;
 $('#intro').backstretch([
-    "img/saga1.jpeg"
-  , "img/saga6.jpeg"
-  , "img/saga8.jpeg"
+  "img/factory2/11.jpg",
+  "img/factory2/3.jpg",  
+  "img/factory2/1.jpg",
+  "img/factory2/4.jpg",
+  "img/Saga Clients/1.jpg",
+  "img/factory2/8.jpg",
+  "img/factory2/13.jpg",
+"img/Saga Clients/6.jpg"
+  
+  
  ], {duration: 3000, fade: 750});
  
 
 window.onload=init;
 function init(){
+
+  var h= document.querySelector('#services').style.height;
+  document.querySelector('#services').querySelector('div').style.height=h;
 
 }
